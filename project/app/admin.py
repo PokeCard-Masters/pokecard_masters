a1ca3dbb0d3664a65ffaf8203b3679f2f4853bae
Agarthaxx <lanllierarthur@gmail.com>
2025-10-01 16:29:40 +0200

    Add_features_rarity_card

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import User
+from .models import User, pokemon
 
 admin.site.register(User)
 # Register your models here.
+admin.site.register(pokemon)
