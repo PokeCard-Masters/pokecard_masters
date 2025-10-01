7c6fa3684688159f3e7ff3fd3b53ad4e2878efa8
fabienstn0 <fabien.stonina@gmail.com>
2025-10-01 14:42:07 +0200

    renaming view from json to ping

--- project/app/urls.py
+++ project/app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("health/", views.json, name="json"),
+    path("health/", views.ping, name="ping"),
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 # Create your views here.
-def json(request):
+def ping(request):
     return JsonResponse({"ping":"pong"})
