dadfb3045142ceb4d7564ff15ed6b889561a213e
Agarthaxx <lanllierarthur@gmail.com>
2025-10-01 13:50:27 +0200

    New class about Pokemon list

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class pokemon(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+class Choice(models.Model):
+    choice_text = models.Charfield(max_lenght=200)
+    search = models.IntegerField(default=0)
