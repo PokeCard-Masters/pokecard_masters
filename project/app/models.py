366292b7f3ccb6a6a2dffff412ca28b2221744e0
Agarthaxx <lanllierarthur@gmail.com>
2025-10-03 10:28:24 +0200

    Ruff format, modification on class models

--- project/app/models.py
+++ project/app/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class pokemon(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=200)
-    card_id = models.CharField(max_length=200)
-    image = models.CharField(max_lenght=200)
+    name = models.CharField(max_length=20)
+    card_id = models.CharField(max_length=10)
+    image = models.CharField(max_length=200)
 
 
 # Create your models here.
