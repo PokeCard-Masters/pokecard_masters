b7553206eccf457efc268f3f5b9ab3cd1eca69cd
LeBaronDeCharlus <git@lebaron.sh>
2026-04-07 14:41:01 +0200

    typo

--- project/app/api.py
+++ project/app/api.py
@@ -253,7 +253,7 @@ def open_booster(request):
         "booster_count": user.booster_count,
     }
 
-@api.get('user/pagination', response={200: list[CardsOut], 404: ErrorOut, 500: TokenOut})
+@api.get('/user/pagination', response={200: list[CardsOut], 404: ErrorOut, 500: TokenOut})
 @paginate
 def pagination(request):
         return Card.objects.all()
